0635cd9840bdeeb561826f9a4db256ded610ef8e
Joshua Dascoli <joshuadascoli@gmail.com>
2025-12-09 17:21:11 -0600

    Fixed for phone screens

--- app/index.tsx
+++ app/index.tsx
@@ -14,16 +14,16 @@ export default function HomeScreen() {
           padding: 20, 
           borderRadius: 10
         }}>
-        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", gap: 100 }}>
-          <Pressable onPress={() => router.push("/singleplayer")} style={{ backgroundColor: "rgb(9, 65, 17)", paddingVertical: 20, paddingHorizontal: 40, borderRadius: 10,}}>
+        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", gap: 20 }}>
+          <Pressable onPress={() => router.push("/singleplayer")} style={{ backgroundColor: "rgb(9, 65, 17)", paddingVertical: 15, paddingHorizontal: 25, borderRadius: 10,}}>
             <Text style={{ fontSize: 24, color: "white", fontWeight: "bold" }}>Singleplayer</Text>
           </Pressable>
-          <Pressable onPress={() => router.push("/multiplayer")} style={{ backgroundColor: "rgb(9, 65, 17)", paddingVertical: 20, paddingHorizontal: 40, borderRadius: 10,}}>
+          <Pressable onPress={() => router.push("/multiplayer")} style={{ backgroundColor: "rgb(9, 65, 17)", paddingVertical: 15, paddingHorizontal: 25, borderRadius: 10,}}>
             <Text style={{ fontSize: 24, color: "white", fontWeight: "bold" }}>Multiplayer</Text>
           </Pressable>
         </View>
 
-        <Pressable onPress={() => router.push("/howToPlay")} style={{ backgroundColor: "rgb(9, 65, 17)", paddingVertical: 20, paddingHorizontal: 40, borderRadius: 10, marginTop: 30}}>
+        <Pressable onPress={() => router.push("/howToPlay")} style={{ backgroundColor: "rgb(9, 65, 17)", paddingVertical: 15, paddingHorizontal: 25, borderRadius: 10, marginTop: 30}}>
           <Text style={{ fontSize: 24, color: "white", fontWeight: "bold" }}>How to Play</Text>
         </Pressable>
       </View>
